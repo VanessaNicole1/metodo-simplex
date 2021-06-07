9e387ba878cfdeda9606d488e45f0d4a1152d22c
Israel Leon <israelpat42@gmail.com>
2021-06-06 23:21:14 -0500

    agreguen lo ultomo

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # metodo-simplex
+
+# deploy 
+
+  create virtual envioromentwith the comand
+
+    WINDOWS
+    python3 -m venv .venv
+    
+    #Execute virtual enviroment
+    .venv\Scripts\activate.bat
+    
+    
+    or
+
+    UNIX
+    virtualenv .venv 
+
+    #execute
+    source .venv/bin/activate
+
+install dependencies with 
+  
+    pip install requeriments.txt
+
+# run local
+
+  Go to metodo-simplex/app   and run the command
+
+    cd /metodo-simplex/app
+  
+    python manage.py runserver
--- app/SimplexMethod/views.py
+++ app/SimplexMethod/views.py
@@ -1,7 +1,88 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .maxi import Tableau
+
+## EXAMPLE
+##t = Tableau([-50,-80])
+##t.add_constraint([1, 2], 120)
+##t.add_constraint([1, 1], 90)
+##print(t.solve())
 # Create your views here.
+variables = 2
+restricts = 2
 
 
 def index(request):
-  return render(request,"index.html")
+    if request.method == "GET":
+        return render(request, "index2.html")
+    elif request.method == "POST":
+        variables = request.POST.get("n_variables")
+        restricciones = request.POST.get("n_restriccion")
+        return HttpResponseRedirect(
+            "/calcular?variables=" + variables + "&restricciones=" + restricciones
+        )
+
+
+def calculo(request):
+    global variables, restricts
+    if request.method == "GET":
+        variables = int(request.GET.get("variables"))
+        restricts = int(request.GET.get("restricciones"))
+        return render(
+            request,
+            "index.html",
+            context={"v": range(variables), "r": range(restricts)},
+        )
+    elif request.method == "POST":
+        objetive = []
+        for i in range(variables):
+            objetive.append(float(request.POST.get("obj-" + str(i))) * -1)
+
+        tabla = Tableau(objetive)
+
+        for j in range(restricts):
+            restric = []
+            for i in range(variables):
+                restric.append(float(request.POST.get("res" + str(j) + "-" + str(i))))
+            rta = float(request.POST.get("res" + str(j) + "-rta"))
+            tabla.add_constraint(restric, rta)
+
+        response = tabla.solve()
+        print(response)  # only for see how return data please delete this
+
+    return render(
+        request, "sol.html", context={"solution": response}
+    )  # iter solution to present response
+
+    # *******EXAMPLE OF RESPONSE**********
+    # [
+    #   {
+    #   'iter': 0,
+    #   'pivot': [-1, -1],
+    #   'tableu': [
+    #     [1.0, -50.0, -80.0, 0.0, 0.0, 0.0],
+    #     [0.0, 1.0, 2.0, 1.0, 0.0, 120.0],
+    #     [0.0, 1.0, 1.0, 0.0, 1.0, 90.0]
+    #     ]
+    #   },
+    #   {
+    #   'iter': 1, 'pivot': [3, 2],
+    #   'tableu': [
+    #     [1.0, -10.0, 0.0, 40.0, 0.0, 4800.0],
+    #     [0.0, 0.5, 1.0, 0.5, 0.0, 60.0],
+    #     [0.0, 0.5, 0.0, -0.5, 1.0, 30.0]
+    #     ]
+    #   },
+    #   {
+    #   'iter': 2,
+    #   'pivot': [2, 3],
+    #   'tableu': [[1.0, 0.0, 0.0, 30.0, 20.0, 5400.0], [0.0, 0.0, 1.0, 1.0, -1.0, 30.0], [0.0, 1.0, 0.0, -1.0, 2.0, 60.0]]
+    #   }
+    # ]
+    #
+    #
+    #
+    #
+
+
+# Create your views here.
