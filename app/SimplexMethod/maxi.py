e6e8f2629cf90972042356e6d1647f0d8be304ca
Israel Leon <israelpat42@gmail.com>
2021-06-07 23:19:21 -0500

    add

--- app/SimplexMethod/maxi.py
+++ app/SimplexMethod/maxi.py
@@ -91,5 +91,5 @@ class Tableau:
             f_rta= self.format_response([c+1,r+2],self.display());
             self.response_iter.append(f_rta);
 
-        self.response_iter['response']=self.final_response
+        self.response_iter.append({'response':self.final_response})
         return self.response_iter
